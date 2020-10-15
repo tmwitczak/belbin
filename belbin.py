89c516c5ab1671d48ca9be67723a8938471e4590
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-10-15 23:01:25 +0200

    Add questions and answers

---
+++ belbin.py
@@ -0,0 +1,262 @@
+# Data
+questions_and_answers = [
+    {
+        "question": "Myślę, że na mój wkład w pracę zespołu składa się...",
+        "answers": {
+            "a": {
+                "description": "moja zdolność szybkiego dostrzegania i wykorzystywania nowych możliwości",
+                "score": 0,
+            },
+            "b": {
+                "description": "umiejętność współpracy z różnego typu osobami",
+                "score": 0,
+            },
+            "c": {
+                "description": "generowanie pomysłów, które jest jedną z moich naturalnych właściwości",
+                "score": 0,
+            },
+            "d": {
+                "description": "to, że potrafię oddać głos innym, jeżeli widzę, że mają coś wartościowego co mogą wnieść do pracy grupy",
+                "score": 0,
+            },
+            "e": {
+                "description": 'moja efektywność, zdolność do "podążania do przodu", powodująca doprowadzanie spraw do końca',
+                "score": 0,
+            },
+            "f": {
+                "description": "jeżeli coś może w efekcie doprowadzić grupę do wartościowego rezultatu robię to, nawet kosztem czasowej niepopularności",
+                "score": 0,
+            },
+            "g": {
+                "description": "moje wyczucie tego, co jest realne i możliwe do wykonania",
+                "score": 0,
+            },
+            "h": {
+                "description": "to, że potrafię bez uprzedzeń czy nastawień wziąć pod uwagę różne, alternatywne metody działania, różne drogi prowadzące do rozwiązań",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "To, co mi głównie przeszkadza w pracy w zespole, to...",
+        "answers": {
+            "a": {
+                "description": "zła organizacja i złe prowadzenie spotkań",
+                "score": 0,
+            },
+            "b": {
+                "description": "gdy osoby, które mają coś ważnego do powiedzenia nie są dopuszczane do głosu przez innych",
+                "score": 0,
+            },
+            "c": {
+                "description": "że gdy pojawią się nowe idee ja zbytnio się rozpalam i rozgaduje, podczas gdy grupa idzie do przodu",
+                "score": 0,
+            },
+            "d": {
+                "description": "mój obiektywny punkt widzenia, który utrudnia mi przyłączenie się z entuzjazmem i pełną gotowością do kolegów",
+                "score": 0,
+            },
+            "e": {
+                "description": "jestem postrzegany jako zbytnio naciskający i autorytarny w sytuacjach, gdy pojawia się potrzeba konkretnych dokonań",
+                "score": 0,
+            },
+            "f": {
+                "description": "jest mi trudno kierować zespołem, może dlatego, że jestem zbytnio wrażliwy na atmosferę grupy",
+                "score": 0,
+            },
+            "g": {
+                "description": "mam skłonność do tego, aby zbytnio angażować się myślowo w nowe pomysły przez to tracę kontakt z tym co się dzieje",
+                "score": 0,
+            },
+            "h": {
+                "description": "moi koledzy postrzegają mnie jako niepotrzebnie zamartwiającego się detalami i rozważaniem ego, że coś się może nie udać",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "Kiedy jestem zaangażowany w projekt wspólnie z innymi:",
+        "answers": {
+            "a": {
+                "description": "zwykle potrafię wpływać na innych bez wywierania presji",
+                "score": 0,
+            },
+            "b": {
+                "description": "moja czujność zapobiega popełnianiu błędów czy zaniedbań",
+                "score": 0,
+            },
+            "c": {
+                "description": "często wywieram nacisk na podejmowanie działań w celu uniknięcia straty czasu lub w trosce o to, aby nie zagubić głównego tematu",
+                "score": 0,
+            },
+            "d": {
+                "description": "można na mnie liczyć, że wniosę coś oryginalnego",
+                "score": 0,
+            },
+            "e": {
+                "description": "jestem zawsze gotów wesprzeć czyjąś sugestię, jeżeli jest ona korzystna dla wspólnego interesu",
+                "score": 0,
+            },
+            "f": {
+                "description": "zawsze poszukuję nowych trendów, świeżych pomysłów, możliwości rozwoju",
+                "score": 0,
+            },
+            "g": {
+                "description": "moja zdolność trzeźwej oceny sytuacji pomaga grupie w podejmowaniu właściwych decyzji",
+                "score": 0,
+            },
+            "h": {
+                "description": "można na mnie polegać, że sprawdzę, czy całość pracy jest dobrze zorganizowana",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "Moje charakterystyczne podejście do pracy w zespole polega na tym, że...",
+        "answers": {
+            "a": {
+                "description": "jestem szczególnie zainteresowany lepszym poznaniem innych osób",
+                "score": 0,
+            },
+            "b": {
+                "description": "nie mam oporów przed rzucaniem wyzwania poglądom innych i byciem w mniejszości w związku z własnymi poglądami",
+                "score": 0,
+            },
+            "c": {
+                "description": "zazwyczaj znajduję dobre argumenty dla obalenia nierozsądnej propozycji",
+                "score": 0,
+            },
+            "d": {
+                "description": "myślę, że mam talent do wdrażania, wprowadzania w życie planów opracowanych przez zespół",
+                "score": 0,
+            },
+            "e": {
+                "description": "mam tendencję do pomijania rzeczy oczywistych i wychodzenia z czymś nieoczekiwanym",
+                "score": 0,
+            },
+            "f": {
+                "description": "wnoszę trochę perfekcjonizmu do każdego działania, jakie podejmuję w zespole",
+                "score": 0,
+            },
+            "g": {
+                "description": "chętnie nawiązuję pożyteczne kontakty poza grupą",
+                "score": 0,
+            },
+            "h": {
+                "description": "chociaż jestem zainteresowany rożnymi punktami widzenia nie waham się z podjęciem decyzji, jeżeli musi być ona podjęta",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "W swoich działaniach szczególnie lubię...",
+        "answers": {
+            "a": {
+                "description": "analizować sytuację i rozważać wszystkie możliwości wyboru",
+                "score": 0,
+            },
+            "b": {
+                "description": "znajdować praktyczne, skuteczne rozwiązania problemów",
+                "score": 0,
+            },
+            "c": {
+                "description": "umacniać dobre więzi w zespole, budować współpracę",
+                "score": 0,
+            },
+            "d": {
+                "description": "mieć możność wywierania wpływu na decyzje",
+                "score": 0,
+            },
+            "e": {
+                "description": "spotykać ludzi, którzy mają coś nowego do zaproponowania",
+                "score": 0,
+            },
+            "f": {
+                "description": "skłaniać innych do porozumienia w sprawie przyjęcia wspólnego kierunku działania",
+                "score": 0,
+            },
+            "g": {
+                "description": "móc skoncentrować się w pełni na swoim wycinku zadań",
+                "score": 0,
+            },
+            "h": {
+                "description": "mieć takie pole działania, które pozwala mi wykorzystać wyobraźni",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "Kiedy nagle dostanę trudne zadanie, mało czasu i nieprzychylnych ludzi",
+        "answers": {
+            "a": {
+                "description": "chciałbym móc zaszyć się w kącie i samemu znaleźć wyjście z impasu przed rozpoczęciem działania",
+                "score": 0,
+            },
+            "b": {
+                "description": "nawiążę natychmiast kontakt z osobami o najbardziej pozytywnym podejściu",
+                "score": 0,
+            },
+            "c": {
+                "description": "szukałbym sposobu na zredukowanie własnego obciążenia zadaniem poprzez dobrze przemyślane rozłożenie go na pozostałych",
+                "score": 0,
+            },
+            "d": {
+                "description": "moje naturalne poczucie presji czasu, czy presji sytuacji daje mi pewność, że zmieszczę się w czasie",
+                "score": 0,
+            },
+            "e": {
+                "description": "wierzę, że pozostanę spokojny i zachowam zdolność do logicznego myślenia",
+                "score": 0,
+            },
+            "f": {
+                "description": "pomimo presji mogę pracować systematycznie trzymając się obranego kierunku",
+                "score": 0,
+            },
+            "g": {
+                "description": "byłbym gotowy przejąć kierownictwo w sytuacji gdy czuje, że grupa nie robi postępów",
+                "score": 0,
+            },
+            "h": {
+                "description": "otworzyłbym dyskusje, aby podzielić się pomysłami i spowodować, by ruszyć coś do przodu",
+                "score": 0,
+            },
+        },
+    },
+    {
+        "question": "Mam problemy w pracy w grupie w związku z tym, że:",
+        "answers": {
+            "a": {
+                "description": "okazuję niecierpliwość w stosunku do tych, którzy blokują pójście do przodu",
+                "score": 0,
+            },
+            "b": {
+                "description": "jestem krytykowany za zbytnią drobiazgowość, powolność w decyzjach, zbytnie analizowane i nieumiejętność oparcia się na własnej intuicji",
+                "score": 0,
+            },
+            "c": {
+                "description": "przywiązuję zbyt dużą wagę do poprawności i jakości, co często opóźnia realizację zadań",
+                "score": 0,
+            },
+            "d": {
+                "description": "mam skłonność do szybkiego nudzenia się, mobilizują mnie do działania tylko intrygujące tematy",
+                "score": 0,
+            },
+            "e": {
+                "description": "mam trudność z rozpoczęciem działania, dopóki cele nie są jasno określone",
+                "score": 0,
+            },
+            "f": {
+                "description": "czasami mam kłopoty z wyjaśnieniem innym złożonych, skomplikowanych pomysłów, które mi się nasunęły",
+                "score": 0,
+            },
+            "g": {
+                "description": "jestem świadomy tego, że wymagam od innych tego, czego sam nie mogę zrobić",
+                "score": 0,
+            },
+            "h": {
+                "description": "waham się wypowiedzieć swoje argumenty, kiedy stoję naprzeciw realnej opozycji",
+                "score": 0,
+            },
+        },
+    },
+]
